67add4e76149bb29befd2a393040a34a79131182
Anchangwan <ssdii66@gmail.com>
2021-11-24 22:25:48 +0900

    detail

--- vanilla-redux/src/routes/Home.js
+++ vanilla-redux/src/routes/Home.js
@@ -1,32 +1,41 @@
 import React from "react";
 import { useState } from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
+import ToDo from "../components/ToDo";
+import { actionCreators} from "../store";
 
-function Home({toDos}){
-    const [text, setText] = useState("");
-    function onChange(e){
-        setText(e.target.value);
-    }
-    function onSubmit(e){
-        e.preventDefault();
-        console.log(text)
-    }
-    return(
-        <div>
-            <h1>To do</h1>
-            <form onSubmit={onSubmit}>
-                <input type="text" value={text} onChange={onChange}/>
-                <button>Add</button>
-            </form>
-            <ul>
-                <li>{JSON.stringify(toDos)}</li>
-            </ul>
-        </div>
-    );
+function Home({ toDos, addToDo}) {
+  const [text, setText] = useState("");
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    addToDo(text);
+    setText("");
+  }
+  return (
+    <div>
+      <h1>To do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map(toDo => <ToDo {...toDo} key={toDo.id}/>)}
+      </ul>
+    </div>
+  );
 }
 
-function mapStateToProps(state, ownProps){
-    return {toDos:state}
+function mapStateToProps(state) {
+  return { toDos: state };
 }
 
-export default connect(mapStateToProps) (Home);
+function mapDispatchToProps(dispetch){
+    return {
+        addToDo: text => dispetch(actionCreators.addToDo(text))
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Home);
